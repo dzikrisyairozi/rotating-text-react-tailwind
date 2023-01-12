dc3ea49fc19bce747d8a92b700360b52167d6635
dikiogres <dikiogres@gmail.com>
2023-01-12 21:37:39 +0700

    feat: convert

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import './App.css'
 function App() {
 
   return (
-    <main className=''>
-  <div className="wrapper">
+    <main className='bg-[#4E6C50] grid place-items-center h-screen'>
+  <div className="wrapper box-content bg-slate-100 h-[70px] flex rounded-lg py-[50px] px-[30px]">
+  {/* <div className="wrapper bg-slate-100 box-content h-16 flex rounded-lg py-[50px] px-[30px]"> */}
        <p className='font-bold'>RPL is</p>
-       <div className="words">
+       <div className="words overflow-hidden">
         <p className='font-bold'>
-           <span className='text-red-500 font-bold'>fantastic</span>
-           <span className='text-blue-500 font-bold'>interesting</span>
-           <span className='text-green-500 font-bold'>awesome</span>
+           <span className='block h-full pl-2 text-red-500 font-bold'>Fantastic</span>
+           <span className='block h-full pl-2 text-orange-500 font-bold'>Awesome</span>
+           <span className='block h-full pl-2 text-yellow-500 font-bold'>Hard</span>
+           <span className='block h-full pl-2 text-green-500 font-bold'>Money</span>
+           <span className='block h-full pl-2 text-blue-500 font-bold'>Family</span>
            </p>
        </div>
    </div>
